3db18787bd2ba9d9aca91d8e6374cc2edfbb5242
Phill <philldamaskin@gmail.com>
2025-06-03 11:17:41 -0400

    working initial design

---
+++ tests/MultithreadedTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Simulation.Tests
+{
+    public class MultithreadedTests : SimulationTests
+    {
+        [Test]
+        public void SystemGroupCreation()
+        {
+            SystemGroup<UpdateMessage> group = new("update group");
+            TimeSystem a = new();
+            group.Add(a);
+            Simulator.Add(group);
+            Simulator.Broadcast(new UpdateMessage(0.1));
+            Assert.That(a.time, Is.EqualTo(0.1));
+            Simulator.Remove(group);
+            Simulator.Broadcast(new UpdateMessage(0.1));
+            Assert.That(a.time, Is.EqualTo(0.1));
+            group.Dispose();
+        }
+
+        [Test]
+        public void MultipleSystems()
+        {
+            SystemGroup<UpdateMessage> group = new("update group");
+            TimeSystem a = new();
+            TimeSystem b = new();
+            group.Add(a);
+            group.Add(b);
+            Simulator.Add(group);
+            Simulator.Broadcast(new UpdateMessage(0.1));
+            Assert.That(a.time, Is.EqualTo(0.1));
+            Assert.That(b.time, Is.EqualTo(0.1));
+            Simulator.Remove(group);
+            group.Dispose();
+        }
+    }
+}
